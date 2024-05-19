19b5d92eab4b81ab4a412afc5521a14056652774
Eva Varona <evalearns@protonmail.com>
2024-05-18 17:01:51 -0700

    Add checkout payment modal state and order-complete-state

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ completeOrderBtn.textContent = "Complete order";
 menuEl.append(completeOrderBtn);
 
 // Add items to Your Order section
-const btnEl = document.querySelectorAll("button");
-function addFoodItem() {
+const btnEl = document.querySelectorAll(".add-btn");
+function renderYourOrderSection() {
     let total = 0;
     btnEl.forEach( (btn) => {
         btn.addEventListener("click", function() {
@@ -67,4 +67,38 @@ function addFoodItem() {
         })
     })
 }
-addFoodItem();
+renderYourOrderSection();
+
+// Unhide Payment Modal when Complete Order button is clicked.
+function openPaymentModal() {
+    completeOrderBtn.addEventListener("click", function() {
+        document.getElementById("payment-container").style.display = "block";
+    })
+};
+openPaymentModal();
+
+// Close Payment Modal and add confirmation 
+const getPaymentBtnEl = document.getElementById("payment-btn");
+function closePaymentModal() {
+    getPaymentBtnEl.addEventListener("click", function(event) {
+        // Close Payment Modal and other sections
+        event.preventDefault();
+        document.getElementById("payment-container").style.display = "none";
+        yourOrderSection.style.display = "none";
+        totalPrice.style.display = "none";
+        completeOrderBtn.style.display = "none";
+
+        // Create confirmation message
+        const customerName = document.getElementById("name").value;
+        const confirmationMsg = document.createElement("div");
+        confirmationMsg.className = "confirmation-msg";
+        confirmationMsg.textContent = `Thanks, ${customerName}! Your order is on the way!`
+        menuEl.append(confirmationMsg);
+
+        // reload page after 5 seconds
+        setTimeout(function() {
+            window.location.reload();
+        }, 5000);
+    })
+}
+closePaymentModal();
